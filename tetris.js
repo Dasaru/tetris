cce0a5aadb8a3e7c7b8f7b7dd5c4f0fcef6e3fc2
Dallas <dberry02jr@yahoo.com>
2022-01-13 15:06:18 -0800

    first commit

---
+++ tetris.js
@@ -0,0 +1,10 @@
+
+const canvas = document.getElementById("tetrisBoard");
+let ctx = canvas.getContext("2d");
+
+clearScreen();
+
+function clearScreen(){
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, 400, 600);
+}
