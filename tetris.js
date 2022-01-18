478b92e71b050c1078fc3f50abda4f445c69836e
Dallas <dberry02jr@yahoo.com>
2022-01-17 22:07:02 -0800

    add drawBoardBackground function and widen board size

--- tetris.js
+++ tetris.js
@@ -4,11 +4,23 @@ const Tetris = (function(){
 const canvas = document.getElementById("tetrisBoard");
 let ctx = canvas.getContext("2d");
 
+let grid = Array(20).fill(Array(10).fill(null));
+
 clearScreen();
+drawBoardBackground();
+
+/*******************
+ * FUNCTIONS
+ *******************/
 
 function clearScreen(){
+	ctx.fillStyle = "#14a1de";
+	ctx.fillRect(0, 0, 500, 600);
+}
+
+function drawBoardBackground(){
 	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, 400, 600);
+	ctx.fillRect(20, 20, 300, 560);
 }
 
 })();
