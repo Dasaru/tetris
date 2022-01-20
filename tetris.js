32460e61965a1654d8fbeed2cba1718ec57cb234
Dallas <dberry02jr@yahoo.com>
2022-01-19 21:23:29 -0800

    add Tetromino shapeList and Tetromino class

--- tetris.js
+++ tetris.js
@@ -4,10 +4,53 @@ const Tetris = (function(){
 const canvas = document.getElementById("tetrisBoard");
 let ctx = canvas.getContext("2d");
 
+const shapeList = [
+	{
+		type: "I",
+		color: "cyan"
+	},
+	{
+		type: "O",
+		color: "yellow"
+	},
+	{
+		type: "T",
+		color: "purple"
+	},
+	{
+		type: "S",
+		color: "green"
+	},
+	{
+		type: "Z",
+		color: "red"
+	},
+	{
+		type: "J",
+		color: "blue"
+	},
+	{
+		type: "L",
+		color: "orange"
+	}
+];
+
 let grid = Array(20).fill(Array(10).fill(null));
 
 clearScreen();
 drawBoardBackground();
+
+/*******************
+ * CLASSES
+ *******************/
+
+class Tetromino {
+	constructor(shapeIndex){
+		this.shapeIndex = shapeIndex;
+		this.type = shapeList[shapeIndex].type;
+		this.color = shapeList[shapeIndex].color;
+	}
+}
 
 /*******************
  * FUNCTIONS
