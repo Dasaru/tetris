a2ac73c27b140222462a9c3ddac1c4c6d08c7da0
Dallas <dberry02jr@yahoo.com>
2022-02-15 11:26:09 -0800

    add sprites, sprite logic, and backup color if sprites don't load

--- tetris.js
+++ tetris.js
@@ -6,16 +6,17 @@ const Tetris = (function(){
  *******************/
 
  class Tetromino {
-	constructor(shapeIndex, active){
+	constructor(shapeId, active){
 		if (typeof active === "boolean" && active){
 			Tetromino.active = this;
 		}
-		this.shapeIndex = shapeIndex;
-		this.type = shapeList[shapeIndex].type;
-		this.color = shapeList[shapeIndex].color;
-		this.shape = shapeList[shapeIndex].shape;
-		this.size = shapeList[shapeIndex].shape.size;
-		this.states = shapeList[shapeIndex].shape.states;
+		this.id = shapeId;
+		this.type = shapeList[shapeId].type;
+		this.color = shapeList[shapeId].color;
+		this.sprite = shapeList[shapeId].sprite;
+		this.shape = shapeList[shapeId].shape;
+		this.size = shapeList[shapeId].shape.size;
+		this.states = shapeList[shapeId].shape.states;
 		this.state = 0;
 		this.pos = {
 			x: 4,
@@ -45,7 +46,7 @@ const Tetris = (function(){
 				let blockPart = this.states[this.state][row][col];
 				
 				if (blockPart === 1 && playfield[row + this.pos.y][col + this.pos.x] === null){
-					playfield[row + this.pos.y][col + this.pos.x] = this.color;
+					playfield[row + this.pos.y][col + this.pos.x] = this.id;
 				} else {
 					if (blockPart === 1) {
 						// console.error("Drawing Tetronimo into another filled block!");
@@ -125,8 +126,10 @@ const Tetris = (function(){
 
 const shapeList = [
 	{
+		id: 0,
 		type: "I",
 		color: "cyan",
+		sprite: null,
 		shape: {
 			size: 4,
 			states: [
@@ -158,8 +161,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 1,
 		type: "O",
 		color: "yellow",
+		sprite: null,
 		shape: {
 			size: 4,
 			states: [
@@ -173,8 +178,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 2,
 		type: "T",
 		color: "purple",
+		sprite: null,
 		shape: {
 			size: 3,
 			states: [
@@ -202,8 +209,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 3,
 		type: "S",
 		color: "green",
+		sprite: null,
 		shape: {
 			size: 3,
 			states: [
@@ -231,8 +240,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 4,
 		type: "Z",
 		color: "red",
+		sprite: null,
 		shape: {
 			size: 3,
 			states: [
@@ -260,8 +271,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 5,
 		type: "J",
 		color: "blue",
+		sprite: null,
 		shape: {
 			size: 3,
 			states: [
@@ -289,8 +302,10 @@ const shapeList = [
 		}
 	},
 	{
+		id: 6,
 		type: "L",
 		color: "orange",
+		sprite: null,
 		shape: {
 			size: 3,
 			states: [
@@ -424,6 +439,29 @@ let buttonPressed = {
 Tetromino.active = getBlock.next().value;
 scoreboard.nextShape = getBlock.next().value;
 
+let img = new Image();
+let spriteWidth = 30;
+img.onload = function(){
+	Promise.all([
+		createImageBitmap(img, 0, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*1, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*2, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*3, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*4, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*5, 0, 30, 30),
+		createImageBitmap(img, spriteWidth*6, 0, 30, 30),
+	]).then(function (images){
+		shapeList[0].sprite = images[0];
+		shapeList[1].sprite = images[1];
+		shapeList[2].sprite = images[2];
+		shapeList[3].sprite = images[3];
+		shapeList[4].sprite = images[4];
+		shapeList[5].sprite = images[5];
+		shapeList[6].sprite = images[6];
+	});
+};
+img.src = "sprites.png";
+
 function animationTick() {
 	clearScreen();
 	drawBackground();
@@ -481,12 +519,21 @@ function drawPlayfield(){
 		for (let col=0; col < playfield[row].length; col++){
 
 			if (playfield[row][col] !== null){
-				ctx.fillStyle = playfield[row][col];
+				
+				let block = shapeList.find(elem => {
+					return elem.id === playfield[row][col];
+				});
+
 				const cubeSize = 30;
 				const xOffset = board.padding + col*cubeSize; // offsets 30 pixels per column
 				const yOffset = board.height - board.padding - (row+1)*cubeSize; // Inverted (start at bottom)
-				
-				ctx.fillRect(xOffset, yOffset, cubeSize, cubeSize);
+
+				if (block.sprite === null){
+					ctx.fillStyle = block.color;					
+					ctx.fillRect(xOffset, yOffset, cubeSize, cubeSize);
+				} else {
+					ctx.drawImage(block.sprite, xOffset, yOffset);
+				}
 			}
 
 		}
@@ -561,12 +608,21 @@ function drawNextBlock() {
 	let yOffset = (board.nextBlock.height / 2) - (15*block.size/2) + 10;
 	for (let row=0; row < block.size; row++){
 		for (let col=0; col < block.size; col++){
-			if (block.states[block.state][row][col] === 1){
-				ctx.fillStyle = block.color;
+
+			if (block.sprite === null){
+				if (block.states[block.state][row][col] === 1){
+					ctx.fillStyle = block.color;
+				} else {
+					ctx.fillStyle = "black";
+				}
+				ctx.fillRect(board.nextBlock.x + (row*15) + xOffset, board.nextBlock.y + (col*15) + yOffset, 15, 15);
 			} else {
-				ctx.fillStyle = "black";
-			}
-			ctx.fillRect(board.nextBlock.x + (row*15) + xOffset, board.nextBlock.y + (col*15) + yOffset, 15, 15);
+				//ctx.drawImage(block.sprite, 0, 0);
+				if (block.states[block.state][row][col] === 1){
+					ctx.drawImage(block.sprite, board.nextBlock.x + (row*30) + xOffset, board.nextBlock.y + (col*30) + yOffset);
+				}
+			}		
+
 		}
 	}
 }
